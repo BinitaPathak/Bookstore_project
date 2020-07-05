5f0fb705561eb30565dc2a7d9d03d6edc3c2c967
binita <binita.pathak47@gmail.com>
2020-07-05 23:29:29 +0530

    created model for cart and bookorders

--- books/models.py
+++ books/models.py
@@ -15,7 +15,6 @@ class Book(models.Model):
     category = models.ManyToManyField(Category)
     book_image = models.ImageField(upload_to='images/', null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=8)
-    # created = models.DateTimeField(default=timezone.now, null=True, blank=True)
     publish_date = models.DateField(default=timezone.now)
     stock = models.IntegerField(default=0)
 
@@ -26,12 +25,38 @@ class Book(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
-    order_date = models.DateField(null=True)
-    payment_type = models.CharField(max_length=100, null=True)
-    payment_id = models.CharField(max_length=100, null=True)
+    order_date = models.DateField(default=timezone.now)
+    # payment_type = models.CharField(max_length=100, null=True)
+    # payment_id = models.CharField(max_length=100, null=True)
+
+    # def add_to_cart(self, book_id):
+    #     book = Book.objects.get(pk=book_id)
+    #     try:
+    #         preexisting_order = BookOrder.objects.get(book=book, cart=self)
+    #         preexisting_order.quantity += 1
+    #         preexisting_order.save()
+    #     except BookOrder.DoesNotExist:
+    #         new_order = BookOrder.objects.create(
+    #             book=book,
+    #             cart=self,
+    #             quantity=1
+    #         )
+    #         new_order.save()
+    #
+    # def remove_from_cart(self, book_id):
+    #     book = Book.objects.get(pk=book_id)
+    #     try:
+    #         preexisting_order = BookOrder.objects.get(book=book, cart=self)
+    #         if preexisting_order.quantity > 1:
+    #             preexisting_order.quantity -= 1
+    #             preexisting_order.save()
+    #         else:
+    #             preexisting_order.delete()
+    #     except BookOrder.DoesNotExist:
+    #         pass
 
 
 class BookOrder(models.Model):
-    book = models.ForeignKey(Book)
-    cart = models.ForeignKey(Cart)
-    quantity = models.IntegerField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
